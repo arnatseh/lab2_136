ac76dc08dc8e9a784ec97a7218739116e3768be8
arnat <arnatzaa1@gmail.com>
2023-11-27 12:00:13 +0700

    first commit

---
+++ lib/pages/fisrtpage.dart
@@ -0,0 +1,31 @@
+import "package:flutter/material.dart";
+
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text ("The Colosseo"),  
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset("img/Coliseo.jpg"),
+            const Text("นี่คือ ไอคอน social"),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.tiktok),
+                Icon(Icons.facebook),
+                Icon(Icons.discord),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );  
+    }   
+}
